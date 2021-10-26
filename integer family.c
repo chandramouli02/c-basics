e8cf63d7a1e0869fc5aa3b19e71c75a81463734f
chandramouli02 <92287994+chandramouli02@users.noreply.github.com>
2021-10-26 10:57:32 +0530

    2lab

---
+++ integer family.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{ 
+long int phy,mat,eng,tot;
+long long int che;
+phy=85567656;
+che=8865694658;
+mat=668766368;
+eng=994376897;
+tot=phy+che+mat+eng;
+printf("marks in physics: %ld\n", phy);
+printf("marks in chemistry: %lld\n", che);
+printf("marks in maths: %ld\n", mat);
+printf("marks in english: %ld\n", eng);
+printf("total marks %lu\n", tot);
+}
